d901b6d026b6757f411a67c4e2025449c2ed20a9
Axel <axel.chauvin@gmail.com>
2025-10-08 15:54:45 +0200

    Implemented reselect for categories selector

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,7 +1,16 @@
-export const selectCategoriesMap = (state) => {
-    return state.categories.categories.reduce((acc, category) => {
-        const {title, items} = category;
-        acc[title.toLowerCase()] = items;
-        return acc;
-    }, {});
-};
+import { createSelector } from "reselect";
+
+const selectCategoriesReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoriesReducer],
+  (categoriesReducer) => categoriesReducer.categories
+);
+
+export const selectCategoriesMap = createSelector([selectCategories], (categories) => {
+  return categories.categories.reduce((acc, category) => {
+    const { title, items } = category;
+    acc[title.toLowerCase()] = items;
+    return acc;
+  }, {});
+});
